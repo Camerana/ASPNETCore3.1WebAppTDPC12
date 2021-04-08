9e8cdcfba6b212b682d8751cf28ec76477c1cf5b
turtleseverywhere <s.samele@libero.it>
2021-04-08 20:18:11 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TDPC12_ASPNETCore3._1WebAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
