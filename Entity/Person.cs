a43f8ae204fe13b47662e7b395cbf310ca359d20
turtleseverywhere <s.samele@libero.it>
2021-04-29 21:21:11 +0200

    nullable

--- Entity/Person.cs
+++ Entity/Person.cs
@@ -4,9 +4,9 @@ namespace TDPC12_ASPNETCore3._1WebAppMVC.Entity
 {
     public class Person
     {
-        public Guid ID { get; set; }
+        public Guid? ID { get; set; }
         public string Nome { get; set; }
         public string Cognome { get; set; }
-        public decimal Stipendio { get; set; }
+        public decimal? Stipendio { get; set; }
     }
 }
