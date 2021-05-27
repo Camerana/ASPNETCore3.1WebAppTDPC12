a1fedb2ea2804220b133d63554e20931f6e57229
turtleseverywhere <s.samele@libero.it>
2021-05-27 20:28:16 +0200

    fix

---
+++ Controllers/APIController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TDPC12_ASPNETCore3._1WebAppMVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class APIController : ControllerBase
+    {
+        [HttpGet("getasync")]
+        public async Task<List<string>> GetAsync()
+        {
+            List<string> result = new List<string>()
+            {
+                "stringa 1",
+                "stringa 2"
+            };
+            return result;
+        }
+    }
+}
